082a0ec733111f810507407a7d317f0b7fc1937c
Nico <nico-braun@live.de>
2020-05-12 22:23:28 +0000

    add error endpoint

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const server = http.Server(app);
 server.listen(port, () => console.log('listenting on port', port));
 app.use(express.json());
 
-app.use('/static', express.static('public'));
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, Azure.' })
+});
 
-app.get('/endpoint', (req, res) => {
-  res.send('hello, azure')
+
+app.get('/error', (req, res) => {
+  try {
+    const wrong = 1
+    wrong = 2
+  } catch (err) {
+    return res.status(500).json({ error: err.message })
+  }
+  res.json({ message: '' })
 });
